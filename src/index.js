8e489bd33ab59d99175508814aee879beaebc5d1
Stefanie <stefcodes@gmail.com>
2020-01-31 13:53:45 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('%c HI-C', 'color: firebrick');
+    fetchDogs()
+  })
+
+  function fetchDogs() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(resp => resp.json())
+    .then(dogs => renderDogs(dogs));
+  }
+
+  function renderDogs(dogs) {
+    const dog_container = document.getElementById('dog-image-container')
+    dogs.forEach(dog => {
+      const img = document.createElement('img')
+      img.src= `${dog}`
+      dog_container.appendChild(img)
+    })
+  }
+
+
+
